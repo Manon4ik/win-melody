66aa17bd27ecd4429b3e0889d1d09d4069faf9e0
Manon4ik <manon4ik@gmail.com>
2016-02-03 17:14:31 +0200

    Начало разработки игры.

---
+++ win-melody/win-melody/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace win_melody
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnParams = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPlay.Location = new System.Drawing.Point(120, 78);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(427, 94);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Игра";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // btnParams
+            // 
+            this.btnParams.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnParams.Location = new System.Drawing.Point(120, 254);
+            this.btnParams.Name = "btnParams";
+            this.btnParams.Size = new System.Drawing.Size(427, 94);
+            this.btnParams.TabIndex = 1;
+            this.btnParams.Text = "Настройки";
+            this.btnParams.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExit.Location = new System.Drawing.Point(120, 428);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(427, 94);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 615);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnParams);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "fMain";
+            this.Text = "Угадай мелодию";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnParams;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
