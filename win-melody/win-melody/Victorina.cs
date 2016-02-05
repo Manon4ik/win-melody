51beccc99e6ac1165a7f76891b69f22f8ac1bd8b
Manon4ik <manon4ik@gmail.com>
2016-02-05 10:45:42 +0200

    Урок 5 (7:11).

--- win-melody/win-melody/Victorina.cs
+++ win-melody/win-melody/Victorina.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
 
 namespace win_melody
 {
     static class Victorina
     {
         static public List<string> list = new List<string>();
+
+        static public int gameDuration = 60;
+        static public int musicDuration = 10;
+        static public bool randomStart = false;
+        static public string lastFolder = "";
+        static public bool allDirectories = false;
+
+        static public void ReadMusic()
+        {
+            string[] music_list = Directory.GetFiles(lastFolder, "*.mp3", allDirectories ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+            list.Clear();
+            list.AddRange(music_list);
+        }
+
+        static string regKeyName = "Software\\MyCompanyName\\Victorina";
+
+
     }
 }
